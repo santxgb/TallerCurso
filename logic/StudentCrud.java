59108fc5fd43d7168e503f7b1057a853f2edd631
santxgb <gabrielsantiagobordacurrea@gmail.com>
2026-03-22 17:25:54 -0500

    Utilicé el método .isblank para el metodo de actualizar con el fin de que en dado caso que el usuario que maneje el crud pueda actualizar los atributos que desee y los no deseados simplemente los skipee con un enter y se mantienen igual. Además ya adapté el main con el fin de probar el primer crud. Por ahora todo muy bien y lo único que me faltaría de Student según yo es cambiar la instrucción para enseñarle al usuario que se debe de dar espacio para seguir con el otro dato sin cambiarlo

--- logic/StudentCrud.java
+++ logic/StudentCrud.java
@@ -61,7 +61,36 @@ public class StudentCrud {
             System.out.println("Este id no está asignado a ningún estudiante");
             return;
         }
-        System.out.println("Este es el Id actual: " + actual);
+        System.out.println("Este es el estudiante actual: " + actual);
+        System.out.println("Nombre a cambiar [ " + actual.getName() + " ]: ");
+        String nombre = scanner.nextLine();
+        if (nombre.isBlank()) nombre = actual.getName();
 
+        System.out.println("Código a cambiar [" + actual.getCode() + " ]: ");
+        String code = scanner.nextLine();
+        if (code.isBlank()) code = actual.getCode();
+
+        System.out.println("Semestre a cambiar [" + actual.getSemester() + " ]: ");
+        String semester = scanner.nextLine();
+        int sem = semester.isBlank() ? actual.getSemester() : Integer.parseInt(semester);
+
+        System.out.println("Promedio a cambiar [" + actual.getAverage() + " ]: ");
+        String avg = scanner.nextLine();
+        double average = avg.isBlank() ? actual.getAverage() : Double.parseDouble(avg);
+
+        if(management.updateObject(new Student(actual.getId(), nombre, code, sem, average)))
+            System.out.println("Actualizado exitosamente");
+        else 
+            System.out.println("No se pudo actualizar el registro del estudiante");
     }
+    private void eliminar(){
+        System.out.println("\n Id a eliminar: ");
+        System.out.println(management.deleteObjetct(scanner.nextLine()) ? "Estudiante Eliminado exitosamente" : "No se encontró el estudiante a eliminar");
+    }
+    private void listar() {
+        if (management.getListObject().isEmpty()) { System.out.println("Sin registros."); return; }
+        management.getListObject().forEach(System.out::println);
+        System.out.println("Total: " + management.getListObject().size());
+    }
+
 }
--- run/Main.java
+++ run/Main.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        StudentCrud studentCrud = new StudentCrud(scanner);
+        String menuPrincipal = """
+            ==================================
+            ||      SISTEMA DE CURSOS       ||
+            ||                              ||
+            ==================================
+            ||  [1] Gestionar Estudiantes   ||
+            ||  [2] Gestionar Profesores    ||
+            ||  [3] Gestionar Cursos        ||
+            ||  [4] Gestionar Electivas C.  ||
+            ||  [5] Gestionar Contenidos    ||
+            ||  [6] Salir                   ||
+            ==================================
+                """;
+        boolean menuWorking = true;
+        do{
+            System.out.println(menuPrincipal);
+            System.out.println("Seleccione lo que desea hacer: ");
+            int option = Integer.parseInt(scanner.nextLine());
+
+            switch (option) {
+                case 1-> studentCrud.menu();
+                default -> System.out.println("Opción no válida.");
+            }
+
+        }while (menuWorking);
     }
 }
